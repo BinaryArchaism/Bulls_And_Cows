46d8bb5e0f6bbd29a61573c366e76b377d1eeb0e
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-01-06 15:58:43 +0300

    v1.0

--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -2,4 +2,9 @@
  * Created by Dmitrii on 06.01.2017.
  */
 public class BullsAndCows {
+    double number;
+
+    public BullsAndCows(double number) {
+        this.number = number;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 /**
  * Created by Dmitrii on 06.01.2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        int bulls = 0;
+        int cows = 0;
+        int random = (int) ((Math.random() * 9000) + 1000);
+        System.out.println(random);
+        Scanner sc = new Scanner(System.in);
+        boolean equal = false;
+        int ra = random/1000;
+        int rb = random/100%10;
+        int rc = random/10%10;
+        int rd = random/1%10;
+
+        do {
+        System.out.println("Введите ваше число");
+        int number = sc.nextInt();
+        int na = number/1000;
+        int nb = number/100%10;
+        int nc = number/10%10;
+        int nd = number/1%10;
+
+        if (na == ra) bulls++;
+        if (na == rb || na == rc || na == rd) cows++;
+        if (nb == rb) bulls++;
+        if (nb == ra || nb == rc || nb == rd) cows++;
+        if (nc == rc) bulls++;
+        if (nc == rb || nc == ra || nc == rd) cows++;
+        if (nd == rd) bulls++;
+        if (nd == rb || nd == rc || nd == ra) cows++;
+        if (bulls != 4) equal= true;
+        System.out.println(bulls+" Быков  "+cows+" Коров");
+        bulls = 0;
+        cows = 0;
+        } while (equal == true);
+    }
 }
